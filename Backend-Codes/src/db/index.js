90227ed053e58103ead189596167cb97e45b9bf9
Akshat Singh <ayushsingh3393@gmail.com>
2025-07-05 20:17:33 +0530

    test.js

--- Backend-Codes/src/db/index.js
+++ Backend-Codes/src/db/index.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
 
+// always remember database connection can cause error so always use try and catch
+// use async await because db is always in other continent.
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
